759282eb40b98e0dbb8bcfaa48ab10fb9494c389
Ivan <ivamonza@gmail.com>
2025-11-26 18:18:33 -0500

    Se programa la primera funcion fibonacci

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+import os
+
+os.system('cls')
+def fibonacci_serie():
+    n = int(input("Ingrese cuántos números de Fibonacci desea imprimir: "))
+
+    a, b = 0, 1
+    print("Serie Fibonacci:")
+
+    for i in range(n):
+        print(a, end=" ")
+        a, b = b, a + b
+print("/n")
+print("/n")
 def rellenar_espacios(texto, tamanio):
     cantidad_espacios = tamanio - len(texto)
     return texto + ' ' * cantidad_espacios
@@ -11,6 +25,8 @@ while True:
     print("║" + 'Seleccione una de las siguientes opciones'.center(64) + "║")
     print("╠" + "═" * 64 + "╣")
     print("║" + rellenar_espacios(' 1. Fibonacci', 64) + "║")
+    
+    
     print("║" + rellenar_espacios(' 2. Factorial', 64) + "║")
     print("║" + rellenar_espacios(' 3. Primos', 64) + "║")
     print("║" + rellenar_espacios(' 4. N números perfectos', 64) + "║")
@@ -19,6 +35,7 @@ while True:
  
     opcion_seleccionada = input('Digite la opcion a elegir: ')
     if (opcion_seleccionada == '1'):
+       fibonacci_serie()
        pass
     elif(opcion_seleccionada == '2'):
        pass
